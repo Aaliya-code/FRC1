8b7172b9eae897913c4d9a6b4e1f71ff6285df9f
Aaliya-code <aalicha72@gmail.com>
2024-07-08 18:58:36 -0500

    Create Day 1.java

---
+++ Day 1.java
@@ -0,0 +1,6 @@
+System.out.println("Hello ITKAN!");
+
+public static void main(String[] args) throws Exception{
+   System.out.println("Hello ITKAN!");
+   System.out.println("I can write a main method!");
+   }
